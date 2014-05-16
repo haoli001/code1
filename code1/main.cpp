9adcd333c827827598a9861643d38a37e12f9c01
李昊 <lihao@lihaodeMacBook-Pro.local>
2014-05-17 00:04:36 +0800

    Initial Commit

---
+++ code1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  code1
+//
+//  Created by 李昊 on 14-5-17.
+//  Copyright (c) 2014年 lihao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
